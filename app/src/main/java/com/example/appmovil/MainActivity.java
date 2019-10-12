19d69231683414fd8b628ffe03c4cb674ca5b90f
GracielaTorrecilla <ge.torrecillas@utadeo.edu.co>
2019-10-11 23:43:54 -0500

    Agrego Mapa, navegabilidad

--- app/src/main/java/com/example/appmovil/MainActivity.java
+++ app/src/main/java/com/example/appmovil/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnEntrar;
     private Button btnRegis;
+    private TextView txtOlv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private void configureBoton(){
 
         btnEntrar = findViewById(R.id.entrar);
-        btnRegis = findViewById((R.id.regis));
+        btnRegis = findViewById(R.id.regis);
+        txtOlv = findViewById(R.id.txtOlvi);
+
 
         btnEntrar.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -31,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnRegis.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, restaurantActivity.class));
+            }
+        });
+        txtOlv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 startActivity(new Intent(MainActivity.this, RegistrarActivity.class));
